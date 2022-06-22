cfa211c3d0de89f55341e14bacbb668891bdbc03
Azybekov Nuralim <nur.cost.lpoi@gmail.com>
2022-06-22 10:42:21 +0600

    mini ref

--- src/js/modules/activateNavItem.js
+++ src/js/modules/activateNavItem.js
@@ -1,13 +1,12 @@
 const activateNavItem = () => {
     let navItem = document.querySelectorAll('.nav__item');
     for (let el of navItem) {
-        if (el.href.indexOf(window.location.pathname) > 0) {
-            el.classList.add('nav__item--active')
+        if (el.href.indexOf(window.location.pathname.split('/')[1]) > 0) {
+            el.classList.add('nav__item--active');
+        } else if (window.location.pathname.length <= 1) {
+            document.querySelector('.nav__konsult').classList.add('nav__item--active')
         }
     }
-
-    console.log(window.location.pathname)
-    console.log('s')
 }
 
 export default activateNavItem;
